8f727610a206702a23438b912f93601eec161ed4
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-09-02 10:15:01 +0700

    Update Tuanminh_Batch_App.cmd

--- project/app/Tuanminh_Batch_App.cmd
+++ project/app/Tuanminh_Batch_App.cmd
@@ -7,10 +7,12 @@ echo What are you want to do?
 echo 1-calculator
 echo 2-i am bored
 echo 3-time now
+echo 4-github
 set /p here="Your choice:"
 if %here%==1 goto calc
 if %here%==2 goto bored
 if %here%==3 goto time
+if %here%==4 start https://github.com/TuanminhDev/Versatile-software
 :calc
 cls
 title Calculator
